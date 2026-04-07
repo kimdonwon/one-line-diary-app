eaf0f37867445121d203328f2cf149ffdd3dce6c
kimdonwon <ehddnjs0728@naver.com>
2026-04-07 12:17:40 +0900

    bump: 버전 업그레이드 (v1.0.9, versionCode 9)

--- app.config.js
+++ app.config.js
@@ -29,7 +29,7 @@ module.exports = {
         backgroundColor: "#FFFFFF"
       },
       softwareKeyboardLayoutMode: "resize",
-      versionCode: 8
+      versionCode: 9
     },
     web: {},
     plugins: [
